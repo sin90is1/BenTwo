3a64def2f9dea61777920ab892eee7d95c4f65e5
Sina Ghavami Adel <sinaghavamy.sg78@gmail.com>
2025-03-16 01:18:42 +0330

    Setup simple grab with MetaXRInteraction
    for my unreal version 5.4.4 the MetaXRInteraction.69.0.0 works fine and it's doc is correct but the 71 set up method is different and lacks some functionality from 72 and 74, also these two are for unreal 5.5 (that I'm thinking of switch to it for the sake of this project because is really different from MetaXRInteraction.69.0.0 but I'm fine till I could make what's in my mind!)
    
    so yeah currently using MetaXRInteraction plugin for hand interaction and OculusHandTools plugin for hand recognition

---
+++ Source/BenTwo/GameMode/Ben2GameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Ben2GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BENTWO_API ABen2GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
